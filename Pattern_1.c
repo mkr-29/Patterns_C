10c10d2df644a7824947092d414db5267e29d5b7
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-11-09 19:32:31 +0530

    Update Pattern_1.c

--- Pattern_1.c
+++ Pattern_1.c
@@ -1,10 +1,26 @@
+//optimised code so that it doesnt take up much area
 #include<stdio.h>
-
 int main(){
     int i,j;
     for(i=1; i<=5; i++){
         for(j=1; j<=5; j++){
             printf("* ");
         }printf("\n");
+    }
+}
+
+
+//non optimised code:
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1; i<=5; i++) //decides no of rows
+    {
+        for(j=1; j<=5; j++) //decides no of cols
+        {
+            printf("* ");  
+        }
+        printf("\n");
     }
 }
