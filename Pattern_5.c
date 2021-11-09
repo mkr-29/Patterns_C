1ec642ba030aae051eed3fd188b84f28fab9387f
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-11-09 19:46:47 +0530

    Create Pattern_5.c

---
+++ Pattern_5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=5; i>=1; i--){
+        for(j=5; j>=1; j--){
+            printf("%d ",j);
+        }printf("\n");
+    }
+}
+
+/////////////OR//////////////////
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=5; i>=1; i--)
+    {
+        for(j=5; j>=1; j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
