0feea26f5fbd8482aec3d079ed649784ef95e3fd
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-11-22 22:11:46 +0530

    Create Pattern_15.c

---
+++ Pattern_15.c
@@ -0,0 +1,17 @@
+int main(){
+    int i,j,k,l;
+    for(i=1; i<=5; i++){
+        k=i;
+        l=5-i+1;
+        for(j=1; j<=5; j++){
+            if(j%2==0){
+                printf("%2d ",k);
+            }
+            else{
+                printf("%2d ",l);
+            }
+            k=k+5;
+            l=l+5;
+        }printf("\n");
+    }
+}
